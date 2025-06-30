4839abeaef74db20b8de1dcc7e3121b1015890ce
Zoltrix <zahlaminn@yandex.ru>
2025-06-30 22:53:16 +1000

    setting up fixers

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,20 +1,31 @@
 <?php
 
-return (new PhpCsFixer\Config())
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__)
+    ->exclude(['var', 'vendor', 'tests/_output'])
+    ->notPath('config/preload.php')
+    ->notPath('public/index.php')
+    ->name('*.php');
+
+return (new Config())
     ->setRules([
         '@PSR12' => true,
-        '@PhpCsFixer' => true,
         'no_unused_imports' => true,
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_extra_blank_lines' => true,
         'line_ending' => true,
         'single_quote' => true,
+        'phpdoc_to_comment' => false,
         'declare_strict_types' => true,
-        'fully_qualified_strict_types' => true,
-        'phpdoc_to_comment' => ['ignored_tags' => ['psalm']],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
     ])
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->in(__DIR__)
-            ->exclude(['vendor', 'var', 'bin'])
-    );
+    ->setFinder($finder)
+    ->setUsingCache(true)
+    ->setRiskyAllowed(true);
